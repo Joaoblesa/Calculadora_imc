0144db5407940806377cd9ddff06b3a9eb966af9
25132585 <silvazxt0@gmail.com>
2026-03-09 16:58:17 -0300

    Fazendo Projeto

---
+++ app/src/main/java/com/aulasandroid/calculadoraimc/MainActivity.kt
@@ -0,0 +1,91 @@
+package com.aulasandroid.calculadoraimc
+
+import android.R.attr.y
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.aulasandroid.calculadoraimc.ui.theme.CalculadoraIMCTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CalculadoraIMCTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    CalculadoraImcScreen(modifier = Modifier.padding(innerPadding))
+
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun CalculadoraImcScreen(modifier: Modifier = Modifier) {
+
+    Column(modifier = modifier.fillMaxSize()) {
+
+        Column(modifier = Modifier
+            .fillMaxWidth()
+            .height(160.dp)
+            .background(color = colorResource(R.color.cor_app)),
+            horizontalAlignment = Alignment.CenterHorizontally
+
+        ) {
+            Image(
+                painter = painterResource(R.drawable.bmi),
+                contentDescription = "Imc Logo",
+                modifier = Modifier.size(80.dp)
+                    .padding(vertical = 16.dp)
+            )
+
+            Text(
+                text = "Calculadora IMC",
+                fontSize = 24.sp,
+                color = Color.White,
+                fontWeight = FontWeight.Bold
+            )
+        }
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 32.dp)
+        ) {
+            Card(
+                modifier = Modifier.fillMaxWidth()
+                    .height(300.dp)
+                    .offset(y = (-30).dp),
+                colors = CardDefaults.cardColors(
+                    containerColor = Color(0xFFF9F6F6)
+                ),
+                elevation = CardDefaults.cardElevation(4.dp)
+            ) { }
+        }
+    }
+}
+
